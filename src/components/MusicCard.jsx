0082e17b5bafecd4065649fc83e4ce7e06177c1b
Vitor <vieira.vitor42@gmail.com>
2022-04-20 10:58:32 -0300

    Req 7

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class MusicCard extends Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <div>
+        <p>
+          { trackName }
+        </p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+};
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,8 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trackList: [],
+      newTrackList: '',
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const retonoAPI = await getMusics(id);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+    const firstObj = retonoAPI.shift();
+    this.setState({
+      trackList: retonoAPI,
+      newTrackList: firstObj,
+    });
+  }
+
   render() {
-    return (<div data-testid="page-album"><Header /></div>);
+    const { trackList, newTrackList } = this.state;
+    return (
+      <div data-testid="page-album">
+        <Header />
+        <div data-testid="artist-name">
+          { newTrackList.artistName }
+        </div>
+        <div data-testid="album-name">
+          { newTrackList.collectionName }
+        </div>
+        <div>
+          { trackList.map((trackMusic, index) => (<MusicCard
+            key={ index }
+            trackName={ trackMusic.trackName }
+            previewUrl={ trackMusic.previewUrl }
+            trackId={ trackMusic.trackId }
+          />
+
+          )) }
+        </div>
+      </div>);
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.string.isRequired,
+  params: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
